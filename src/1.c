c4932170b3a93f6d6f917861912b8829d64e8779
gallouxpatcho <gallouxpatcho@gmail.com>
2025-03-06 13:36:40 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,10 @@
+
+int main() {
+  int x = 5;
+  float y = 3.14;
+  char z = 'a';
+  printf("The value of x is %d\n", x);
+  printf("The value of y is %f\n", y);
+  printf("The value of z is %c\n", z);
+  return 0;
+}
